405d17cc6839d3d89c633ca30e0691a73fd50c79
Yunyang-Fei <f.lucky@wustl.edu>
2025-09-17 13:50:06 -0500

    Update Sieve of Eratosthenes.java

--- src/studio3/Sieve of Eratosthenes.java
+++ src/studio3/Sieve of Eratosthenes.java
@@ -1,17 +1,36 @@
-public static void int sieveofEratosthenes(int[][]array, int n) {
-    boolean prime[] = new boolean[n + 1];
-    for (int i = 0; i <= n; i++)
-        prime[i] = true;
+package studio3;
 
-    for (int p = 2; p * p <= n; p++) {
-        if (prime[p] == true) {
-            for (int i = p * p; i <= n; i += p)
-                prime[i] = false;
+import java.util.Scanner;
+
+public class SieveOfEratosthenes {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = in.nextInt();
+        System.out.println("Prime numbers up to " + n + " are:");
+        sieveOfEratosthenes(n);
+    }
+
+// Prints all prime numbers smaller than or equal to n
+    public static void sieveOfEratosthenes(int n) {
+        if (n < 2) {
+            System.out.println("No primes <= " + n);
+            return;
         }
-    }
 
-    for (int p = 2; p <= n; p++) {
-        if (prime[p] == true)
-            System.out.print(p + " ");
-    }
-}   
+        boolean[] prime = new boolean[n + 1];
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
+
+        prime[0] = false;
+        prime[1] = false;
+
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p]) {
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+
+}
+}
