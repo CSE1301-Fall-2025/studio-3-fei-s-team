e5a08a225df0e01a50847242cccd62471a6d51ae
Yunyang-Fei <f.lucky@wustl.edu>
2025-09-17 13:30:25 -0500

    Update Sieve of Eratosthenes.java

--- src/studio3/Sieve of Eratosthenes.java
+++ src/studio3/Sieve of Eratosthenes.java
@@ -1,28 +1,17 @@
+public static void int sieveofEratosthenes(int[][]array, int n) {
+    boolean prime[] = new boolean[n + 1];
+    for (int i = 0; i <= n; i++)
+        prime[i] = true;
 
-public static void main(String[] args) {
-    int n = 40; 
-    System.out.println("Number of primes up to " + n + " is: " + sieveOfEratosthenes(n));
-
-
-if (n >= 2) {
-boolean prime[] = new boolean[n + 1];
-for (int i = 0; i <= n; i++)
-    prime[i] = true;
-for (int p = 2; p * p <= n; p++) {
-    if (prime[p] == true) {
-        for (int i = p * p; i <= n; i += p)
-            prime[i] = false;
-           
-}
-}
-int count = 0;
-for (int p = 2; p <= n; p++) {
-    if (prime[p]) {
-        count++;
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
     }
-}
-}
-}
-    else {
-        System.out.println("There are no prime numbers less than 2.");
+
+    for (int p = 2; p <= n; p++) {
+        if (prime[p] == true)
+            System.out.print(p + " ");
     }
+}   
